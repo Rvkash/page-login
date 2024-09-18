b819c3accdec7ef9424971608b804263c6a72dd4
Rafael Linhres <rafael.ieel@hotmail.com>
2024-09-18 20:32:24 -0300

    add: inputs,imgs,button

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,15 +1,27 @@
 import "./style.css";
+import Trash from "../../assets/trash.svg";
 
 function Home() {
   return (
     <div className="container">
       <form>
-        <h1>Cadastro de usuários</h1>
+        <h1>Cadastro de Usuários</h1>
         <input name="nome" type="text" />
         <input name="idade" type="number" />
         <input name="email" type="email" />
         <button type="button">Enviar</button>
       </form>
+
+      <div>
+        <div>
+          <p>Nome:</p>
+          <p>Idade:</p>
+          <p>Email</p>
+        </div>
+        <button>
+          <img src={Trash} />
+        </button>
+      </div>
     </div>
   );
 }
