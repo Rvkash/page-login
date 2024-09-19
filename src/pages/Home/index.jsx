c4052d9aea9a085a64db15215f2ec7af1349954f
Rafael Linhres <rafael.ieel@hotmail.com>
2024-09-18 21:51:13 -0300

    add: cards

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -2,25 +2,44 @@ import "./style.css";
 import Trash from "../../assets/trash.svg";
 
 function Home() {
-  return (
-    <div className="container">
-      <form>
-        <h1>Cadastro de Usuários</h1>
-        <input name="nome" type="text" />
-        <input name="idade" type="number" />
-        <input name="email" type="email" />
-        <button type="button">Enviar</button>
-      </form>
+  const users = [
+    {
+      id: "45345345345",
+      name: "Rodolfo",
+      age: "33",
+      email: "rodolfo@gmail.com",
+    },
+    {
+      id: "89898989",
+      name: "leticia",
+      age: "23",
+      email: "leticia@gmail.com",
+    },
+  ];
 
-      <div>
-        <div>
-          <p>Nome:</p>
-          <p>Idade:</p>
-          <p>Email</p>
-        </div>
-        <button>
-          <img src={Trash} />
-        </button>
+  return (
+    <div className="container1">
+      <div className="container">
+        <form>
+          <h1>Cadastro de Usuários</h1>
+          <input placeholder="Nome..." name="nome" type="text" />
+          <input placeholder="Idade..." name="idade" type="number" />
+          <input placeholder="Email..." name="email" type="email" />
+          <button type="button">Enviar</button>
+        </form>
+
+        {users.map((user) => (
+          <div key={user.id} className="card">
+            <div>
+              <p>Nome: {user.name}</p>
+              <p>Idade: {user.age}</p>
+              <p>Email: {user.email}</p>
+            </div>
+            <button>
+              <img src={Trash} />
+            </button>
+          </div>
+        ))}
       </div>
     </div>
   );
